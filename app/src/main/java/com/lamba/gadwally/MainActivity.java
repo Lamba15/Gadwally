25be0971a2f253b71b65dbe0fcc4eda3dad9ca6b
Whelllava <whelllava@gmail.com>
2018-02-04 17:37:31 +0200

    Basha Shof Kda wi 2oli :)

--- app/src/main/java/com/lamba/gadwally/MainActivity.java
+++ app/src/main/java/com/lamba/gadwally/MainActivity.java
@@ -1,18 +1,76 @@
 package com.lamba.gadwally;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button login;
+    Dialog regdialog;
+    TextView createaccount, desc1 , desc2, forgetpass;
+    Typeface face;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+        face = Typeface.createFromAsset(getAssets(),"fonts/bauhaus93.ttf");
+        login = findViewById(R.id.button);
+        createaccount = findViewById(R.id.create_account);
+        forgetpass = findViewById(R.id.forget_password);
+        desc1 = findViewById(R.id.mydesc);
+        desc1.setTypeface(face);
+        desc2 = findViewById(R.id.mydesc2);
+        desc2.setTypeface(face);
+        regdialog = new Dialog(this);
+        forgetpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "lma nb2a nensa el password", Toast.LENGTH_SHORT).show();
+            }
+        });
+        createaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView close;
+                Button submit;
+                regdialog.setContentView(R.layout.activity_register);
+                close = regdialog.findViewById(R.id.cancel_cross);
+                submit = regdialog.findViewById(R.id.submit_register);
+                close.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        regdialog.dismiss();
+                    }
+                });
+                submit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, " لسه فاضل شوية شغل ", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                regdialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
+                regdialog.show();
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Dos 3la Create Account kda", Toast.LENGTH_SHORT).show();
 
+            }
+        });
 
 
     }
---
+++ app/src/main/java/com/lamba/gadwally/Register.java
@@ -0,0 +1,24 @@
+package com.lamba.gadwally;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Register extends Activity {
+    ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+
+
+}
+
