8542ba10d0feaba7e8058721cb55bfff479ce6aa
Whelllava <whelllava@gmail.com>
2018-02-12 19:03:40 +0200

    Dah El Home Shof Kda wi 2oli

---
+++ app/src/main/java/com/lamba/gadwally/Interface.java
@@ -0,0 +1,20 @@
+package com.lamba.gadwally;
+
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+
+//public class Interface extends AppCompatActivity {
+////    ViewPager pager;
+////    ViewPageAdabter myadabter;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.app_bar_main__interface);
+////        pager = findViewById(R.id.viewpager);
+////        myadabter = new ViewPageAdabter(getApplicationContext());
+////        pager.setAdapter(myadabter);
+//    }
+//}
+//
--- app/src/main/java/com/lamba/gadwally/Register.java
+++ app/src/main/java/com/lamba/gadwally/Register.java
@@ -1,12 +1,8 @@
 package com.lamba.gadwally;
 
-import android.app.ActionBar;
+
 import android.app.Activity;
-import android.app.Dialog;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 public class Register extends Activity {
     ;
